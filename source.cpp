2d22ea75446d52eefba0c4c3ea8026128433b4b0
Kaiser <khanhphamdh17th08@gmail.com>
2020-10-07 15:46:04 +0700

    da them hang hieu

--- source.cpp
+++ source.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b)
+{
+	int hieu = 0;
+	hieu = a/b;
+	return hieu;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
